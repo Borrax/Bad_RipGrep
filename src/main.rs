451cfa83cd28de012b17b04d836e5653c0807edd
Borrax <borrax@protonmail.com>
2026-03-23 14:57:15 +0200

    Abstracting the finding argument flag logic and panic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::env;
 
+fn is_flag(arg: &str) -> bool {
+    let Some(first_char) = arg.chars().next() else {
+        panic!("Didn't receive a string argument")
+    };
+    first_char == '-' 
+}
+
 fn main() {
     let string_stuff = String::new();
 
     let args: Vec<String> = env::args().collect();
         for (i, arg) in args.iter().enumerate().skip(1) {
             println!("Argument {} {}", i, arg);
-            if let Some(first_char) = arg.chars().next() && first_char == '-' {
-                println!("Found a flag");
-            }
+                if is_flag(arg) {
+                    println!("Found a flag")
+                }
         }
 }
