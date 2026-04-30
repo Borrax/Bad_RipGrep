6903612818a10a71c1b8fd0585151735985a68a2
Borrax <borrax@protonmail.com>
2026-04-30 22:59:11 +0300

    Benchmarking the application

--- benches/bad_ripgrep.rs
+++ benches/bad_ripgrep.rs
@@ -1,2 +1,16 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 
+use regex::Regex;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use bad_ripgrep::run_application;
+
+fn bench_bad_ripgrep(c: &mut Criterion) {
+    let search_re = Regex::new("ipsum").unwrap();
+    c.bench_function("bad_ripgrep run", |b| {
+        b.iter(|| run_application(black_box(&search_re)))
+    });
+}
+
+criterion_group!(benches, bench_bad_ripgrep);
+criterion_main!(benches);
