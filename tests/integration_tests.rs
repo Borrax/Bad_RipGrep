cba751d95a5a6182bc76d21688a9ea3a4244e33b
Borrax <borrax@protonmail.com>
2026-05-02 23:09:00 +0300

    First integreation test under development

---
+++ tests/integration_tests.rs
@@ -0,0 +1,40 @@
+use std::sync::{Arc, Mutex};
+use regex::Regex;
+use bad_ripgrep::run_application;
+
+
+const MAX_WORDS_AROUND_MATCH: usize = 5;
+
+fn assert_surrounding_words_count(text: &str, search_word: &str) {
+    let splitted_text: Vec<&str> = text.split(search_word).collect();
+    let words_before = splitted_text[0].split_whitespace().collect::<Vec<&str>>();
+    assert!(words_before.len() <= MAX_WORDS_AROUND_MATCH + 1); // +1 because it will
+                                                               // include the line index
+
+    let words_after = splitted_text[1].split_whitespace().collect::<Vec<&str>>();
+    assert!(words_after.len() <= MAX_WORDS_AROUND_MATCH);
+}
+
+#[test]
+fn test_more_matches() {
+    let chars = ['i', 'p', 's', 'u', 'm'];
+    let search_word: String = String::from_iter(chars);
+    let search_re = Regex::new(&search_word).unwrap();
+
+    let out = Arc::new(Mutex::new(Vec::new()));
+    run_application(&search_re, out.clone());
+
+    let result = String::from_utf8(out.lock().unwrap().to_vec()).unwrap();
+    let lines: Vec<&str> = result.split("\n").collect();
+    // last line is empty
+    assert_eq!(lines.len(), 9); // including the generated from the benchmarks
+
+    // last line is empty
+    for line in &lines[1..lines.len() - 1] {
+        // assert!(line.contains(&search_word));
+        println!("{}", line);
+        
+        // assert_surrounding_words_count(line, &search_word);
+
+    }
+}   
