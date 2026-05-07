25899360095e9a3ffa49e75d3b1dfe6e2b191a74
Borrax <borrax@protonmail.com>
2026-05-07 23:03:11 +0300

    Defining a function to run the app for the integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -15,16 +15,20 @@ fn assert_surrounding_words_count(text: &str, search_word: &str) {
     assert!(words_after.len() <= MAX_WORDS_AROUND_MATCH);
 }
 
+fn run_and_get_result(search_re: &Regex) -> String {
+    let out = Arc::new(Mutex::new(Vec::new()));
+    run_application(search_re, out.clone());
+
+    return String::from_utf8(out.lock().unwrap().to_vec()).unwrap();
+}
+
 #[test]
 fn test_more_matches() {
     let chars = ['i', 'p', 's', 'u', 'm'];
     let search_word: String = String::from_iter(chars);
     let search_re = Regex::new(&search_word).unwrap();
 
-    let out = Arc::new(Mutex::new(Vec::new()));
-    run_application(&search_re, out.clone());
-
-    let result = String::from_utf8(out.lock().unwrap().to_vec()).unwrap();
+    let result = run_and_get_result(&search_re);
     let lines: Vec<&str> = result.split("\n").collect();
     // last line is empty
     assert_eq!(lines.len(), 9); // including the generated from the benchmarks
@@ -45,9 +49,7 @@ fn test_non_existent_word() {
     let search_word = String::from_iter(chars);
     let search_re = Regex::new(&search_word).unwrap();
 
-    let out = Arc::new(Mutex::new(Vec::new()));
-    run_application(&search_re, out.clone());
-    let result = String::from_utf8(out.lock().unwrap().to_vec()).unwrap();
+    let result = run_and_get_result(&search_re);
 
     assert_eq!("", result);
 }
